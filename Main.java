328a397553a886cf7760bdd9049e45d44333b5c9
LucasOsorno <150816146+LucasOsorno@users.noreply.github.com>
2025-03-02 12:08:08 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package pkg6a;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserWindow userWindow = new UserWindow();
+        userWindow.setVisible(true);
+    }
+    
+}
